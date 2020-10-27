083d2ca57064a2645c801c95913537f35241bc14
Zedination <50323429+Zedination@users.noreply.github.com>
2020-10-27 23:11:22 +0700

    Initial commit

---
+++ src/main/java/vn/edu/utc/tracuu/TracuuApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.utc.tracuu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracuuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TracuuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/edu/utc/tracuu/TracuuApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.utc.tracuu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TracuuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
