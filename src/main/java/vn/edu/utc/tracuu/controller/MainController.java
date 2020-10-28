8c68297a972921fa58fd47dbabb4ae9abbc71b79
Zedination <50323429+Zedination@users.noreply.github.com>
2020-10-28 20:42:18 +0700

    Update MainController.java

--- src/main/java/vn/edu/utc/tracuu/controller/MainController.java
+++ src/main/java/vn/edu/utc/tracuu/controller/MainController.java
@@ -12,7 +12,7 @@ public class MainController {
     @GetMapping(path = {"/","/index"})
     public String getIndex(Model model){
         LocalDate today = LocalDate.now();
-        model.addAttribute("title","ra cứu kết quả phân loại tiếng anh sinh viên hệ đại học chính quy khóa 61 năm "+today.getYear());
+        model.addAttribute("title","Tra cứu kết quả phân loại tiếng anh sinh viên hệ đại học chính quy khóa 61 năm "+today.getYear());
         return "index";
     }
 }
